60efd5949520724c500a8cfc7053f36fc97140f2
Scott <senghuotlim@gmail.com>
2024-08-07 15:13:42 -0700

    Add logging

--- src/main/java/org/example/coffee/ControllerView.java
+++ src/main/java/org/example/coffee/ControllerView.java
@@ -5,6 +5,8 @@ import io.netty.util.internal.StringUtil;
 import org.example.coffee.record.JokeResponse;
 import org.example.coffee.record.JokeResponses;
 import org.example.coffee.service.Redis;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,7 @@ import java.util.Random;
 @Controller
 public class ControllerView {
 
+    private static final Logger logger = LoggerFactory.getLogger(Redis.class);
     Gson gson = new Gson();
     Random random = new Random();
 
@@ -40,14 +43,19 @@ public class ControllerView {
 
             var jokeResponses = gson.fromJson(result.getBody(), JokeResponses.class);
             var jokes = new ArrayList<String>();
-            for (var i = 1; i < jokeResponses.results.length; i++)
-                jokes.add(gson.toJson(jokeResponses.results[i]));
+            for (var i = 1; i < jokeResponses.results.length; i++) {
+                var curJoke = jokeResponses.results[i];
+                var json = gson.toJson(curJoke);
+                logger.info("json: " + json);
+                jokes.add(json);
+            }
             Redis.push("Jokes", jokes.toArray(new String[0]));
 
             end = System.currentTimeMillis();
             joke = jokeResponses.results[0];
         } else {
             end = System.currentTimeMillis();
+            logger.info("response: " + response);
             joke = gson.fromJson(response, JokeResponse.class);
             cacheHit = true;
         }
